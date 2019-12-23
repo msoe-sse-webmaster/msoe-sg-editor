9582a715016cae4a255f2b2776936c2ddd56d1fb
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-12-23 17:13:39 -0600

    Adding heroku as a development host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << 'msoe-sg-editor.herokuapp.com'
   config.github_repo_name = 'msoe-sg-editor-test-repo'
 end
